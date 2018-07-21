5558f2759bfbf6b40cc76b1bfa12660667bd6020
Jose Agapito Ramirez <agapox@gmail.com>
2018-07-21 14:49:55 -0400

    init config

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Components
+import MainContainer from './Components/MainContainer/MainContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <MainContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
